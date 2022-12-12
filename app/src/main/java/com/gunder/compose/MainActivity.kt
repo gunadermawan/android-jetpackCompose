ad2771977707fabd72521e32fc470c206b63d66c
gunadermawan <gunadermawan.official@gmail.com>
2022-12-12 22:33:40 +0700

    insert image and using Row

--- app/src/main/java/com/gunder/compose/MainActivity.kt
+++ app/src/main/java/com/gunder/compose/MainActivity.kt
@@ -3,7 +3,9 @@ package com.gunder.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
@@ -11,6 +13,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.gunder.compose.data.User
 import com.gunder.compose.ui.theme.ComposeUiTheme
@@ -33,6 +36,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(user: User) {
+    Row(modifier = Modifier.padding(12.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_android),
+            contentDescription = "android logos"
+        )
+    }
     Column(modifier = Modifier.padding(12.dp)) {
         Text(text = user.name)
         Text(text = user.profession)
